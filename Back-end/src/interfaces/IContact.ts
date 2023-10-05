93d1a5f53f0f19ead21f5e23fb1636e855d44d11
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-10-05 15:54:14 -0300

    fixes

--- Back-end/src/interfaces/IContact.ts
+++ Back-end/src/interfaces/IContact.ts
@@ -1,11 +1,11 @@
 interface IContact {
-  id?: number;
-  nome?: string;
-  tel_prin?: string;
-  tel_sec?: string;
-  descricao?: string;
-  url_foto?: string;
-  favorito?: boolean;
+  id: number;
+  nome: string;
+  tel_prin: string;
+  tel_sec: string;
+  descricao: string;
+  url_foto: string;
+  favorito: boolean;
 }
 
 export default IContact;
